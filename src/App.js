84640a803a1f0ced6d706f7cf801e86cde57cc8b
Gl1tch42 <michelfc42@gamil.com>
2024-12-16 18:10:15 -0300

    ajustes com login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/searchBar/SearchBar';
 import MovieList from './components/movieList/MovieList';
+import LoginPage from './components/login/Login';
+import Profile from './components/profile/Profile';
 import { FavoriteProvider } from './components/favoriteContext/FavoriteContext';
 import styled from 'styled-components';
 
@@ -17,13 +19,28 @@ const Header = styled.h1`
   margin-bottom: 20px;
 `;
 
+const Superior = styled.div` 
+  width: 100%; 
+  display: flex; 
+  justify-content: flex-end; 
+  padding: 10px;
+`;
+
 function App() {
   const [movies, setMovies] = React.useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const authStatus = localStorage.getItem('isAuthenticated');
+    if (authStatus === 'true') {
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   const handleSearch = async (query) => {
-    const apiKey = 'sua api key';
+    const apiKey = '18dc4cb389aac32424a96a41e455e924';
     const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
-    
+
     const searchResponse = await fetch(searchUrl);
     const searchData = await searchResponse.json();
     console.log(searchData)
@@ -37,16 +54,30 @@ function App() {
         trailer: trailerData.results.length > 0 ? `https://www.youtube.com/watch?v=${trailerData.results[0].key}` : null,
       };
     }));
-    
+
     setMovies(moviesWithTrailers);
+  };
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => { 
+    localStorage.removeItem('isAuthenticated'); 
+    setIsAuthenticated(false); 
   };
 
   return (
     <FavoriteProvider>
       <AppContainer>
-        <Header>Where is my movie ?</Header>
-        <SearchBar onSearch={handleSearch} />
-        <MovieList movies={movies} />
+        {!isAuthenticated ? (<LoginPage onLogin={handleLogin} />) : (<>
+          <Superior>
+            <Profile onLogout={handleLogout} />
+          </Superior>
+          <Header>Where is my movie ?</Header>
+          <SearchBar onSearch={handleSearch} />
+          <MovieList movies={movies} />
+        </>)}
       </AppContainer>
     </FavoriteProvider>
   );
